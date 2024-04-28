f9f011cea44f656972c30e19256f09f0450c8332
Shailesh Shenoy <shenoy.sh@northeastern.edu>
2024-04-28 09:55:28 -0400

    Changed project name

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,62 +8,70 @@ import {
   Stack,
   Text,
   useBreakpointValue,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 
-import NextLink from 'next/link';
+import NextLink from "next/link";
 
-export default function SplitScreen() {
+export default function HomePage() {
   return (
-    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-      <Flex p={8} flex={1} align={'center'} justify={'center'}>
-        <Stack spacing={6} w={'full'} maxW={'lg'}>
-          <Heading fontSize={{ base: '3xl', md: '5xl', lg: '7xl' }}>
+    <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={6} w={"full"} maxW={"lg"}>
+          <Heading fontSize={{ base: "3xl", md: "5xl", lg: "7xl" }}>
             <Text
-              as={'span'}
-              position={'relative'}
+              as={"span"}
+              position={"relative"}
               zIndex={1}
               _after={{
                 content: "''",
-                width: 'full',
-                height: useBreakpointValue({ base: '20%', md: '30%' }),
-                position: 'absolute',
+                width: "full",
+                height: useBreakpointValue({ base: "20%", md: "30%" }),
+                position: "absolute",
                 bottom: 1,
                 left: 0,
-                bg: 'purple.500',
+                bg: "purple.500",
                 zIndex: -1,
-              }}>
+              }}
+            >
               Secure
-            </Text>
-            {' '}
-            <Text color={'purple.500'} as={'span'}>
-              Share
-            </Text>{' '}
+            </Text>{" "}
+            <Text color={"purple.500"} as={"span"}>
+              Social Share
+            </Text>{" "}
           </Heading>
-          <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.700'}>
-            Encrypt, store, & share sensitive data with strict access-control guardrails usiing trust-minimized technologies.
+          <Text fontSize={{ base: "md", lg: "lg" }} color={"gray.700"}>
+            Encrypt, store, & share sensitive data with your social circle.
+            Guardrailed by strict access-control using trust-minimized
+            technologies.
           </Text>
-          <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
-            <Button as={NextLink}
-              href={'/citizen'}
-              rounded={'full'}
-              bg={'purple.500'}
-              color={'white'}
+          <Stack direction={{ base: "column", md: "row" }} spacing={4}>
+            <Button
+              as={NextLink}
+              href={"/citizen"}
+              rounded={"full"}
+              bg={"purple.500"}
+              color={"white"}
               _hover={{
-                bg: 'purple.700',
-              }}>
+                bg: "purple.700",
+              }}
+            >
               Secure my data
             </Button>
-            <Button rounded={'full'} as={NextLink} href="/consumer" variant="ghost" colorScheme='purple' _hover={{ bg: 'purple.100' }}
-            >Access encrypted data</Button>
+            <Button
+              rounded={"full"}
+              as={NextLink}
+              href="/consumer"
+              variant="ghost"
+              colorScheme="purple"
+              _hover={{ bg: "purple.100" }}
+            >
+              Access encrypted data
+            </Button>
           </Stack>
         </Stack>
       </Flex>
       <Flex flex={1}>
-        <Image
-          alt={'Security image'}
-          objectFit={'cover'}
-          src="/hero.jpg"
-        />
+        <Image alt={"Security image"} objectFit={"cover"} src="/hero.jpg" />
       </Flex>
     </Stack>
   );
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,36 @@
-import { Box, Flex, Link, Spacer, Text } from '@chakra-ui/react';
-import NextLink from 'next/link';
+import { Box, Flex, Link, Spacer, Text } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 const Navbar: React.FC = () => {
-    return (
-        <Flex bg="purple.500" p={4} color="white" justify="space-between" textTransform="uppercase" alignItems="center">
-            <Link as={NextLink} href="/" flex={2} fontWeight={600} fontSize={24} fontFamily={"heading"}>Secure Share</Link>
-            <Flex flex={1} justify="space-around" fontSize={18} fontWeight={500}>
-                <Link as={NextLink} href="/citizen">Citizens</Link>
-                <Link as={NextLink} href="/consumer">Data Utilizers</Link>
-            </Flex>
-        </Flex>
-    );
+  return (
+    <Flex
+      bg="purple.500"
+      p={4}
+      color="white"
+      justify="space-between"
+      textTransform="uppercase"
+      alignItems="center"
+    >
+      <Link
+        as={NextLink}
+        href="/"
+        flex={2}
+        fontWeight={600}
+        fontSize={24}
+        fontFamily={"heading"}
+      >
+        Secure Social Share
+      </Link>
+      <Flex flex={1} justify="space-around" fontSize={18} fontWeight={500}>
+        <Link as={NextLink} href="/citizen">
+          Citizens
+        </Link>
+        <Link as={NextLink} href="/consumer">
+          Data Utilizers
+        </Link>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Navbar;
